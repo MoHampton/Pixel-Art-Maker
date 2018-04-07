18f0f1a55aac31eaee4210b0d5b8fe61c7733dbb
Mo Hampton <monique.j.hampton@gmail.com>
2018-04-07 17:58:12 -0400

    First Cut

---
+++ designs.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+// Variable: Select color 
+  let gridCol = $("#colorPicker").val();
+// Variable: Select grid size
+  let rowCount = $("#inputHeight").val();
+  let colCount = $("#inputWidth").val();
+// When size is submitted by the user, call makeGrid()
+  $("#submit").click(function() {
+    makeGrid();
+  });
+// Event listener to add color
+  $("table").on("click", "td", function() {
+    gridCol = $("#colorPicker").val();
+    $(this).css("background-color", gridCol);
+  });
+
+  function makeGrid() {
+    resetGrid(); //Resets the grid and removes old pixel art
+    //Creates table row parents
+    for (let rows = 0; rows < rowCount; rows++) {
+      let gridVal = $('<tr></tr>');
+      $('#pixelCanvas').append('<tr class="gridRow"></tr>');
+      //alert(gridVal);
+    }
+    //Creates and append tabe data (columns) into parent table rows
+    for (let cols = 0; cols < colCount; cols++) {
+      $('tr').append('<td class="gridCol"></td>');
+    }
+  }
+
+  //Resets grid without refreshing the page
+  function resetGrid() {
+    rowCount = $('#inputHeight').val(); //resets height
+    colCount = $('#inputWidth').val(); //resets width 
+    $('.gridRow').remove();//removes old grid
+  }
+});
